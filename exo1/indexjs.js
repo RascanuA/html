5877ade5ee11642f140ddf7a4ac52f2b40a8e319
RascanuA <rascanuandreea86@gmail.com>
2023-10-19 09:46:54 +0200

    .

---
+++ exo1/indexjs.js
@@ -0,0 +1,4 @@
+let largeur = parseInt(prompt('Quel est la largeur?'));
+let longueur = parseInt(prompt('Quel est la longeur?'));
+let surface =  largeur*longueur; 
+alert(`L'aire du rectangle est  ${surface}`);
---
+++ tp1/second.js
@@ -0,0 +1,7 @@
+let prenom = prompt('Quel est votre prénom?');
+let nom = prompt('Quel est votre nom?');
+let age = prompt('Quel est votre prénom?');
+// cu acest cod  programul citeste un sir  si nu face calculul (let ageFutur=age+1)
+let ageFutur= parseInt (age)+1; 
+// parseInt citeste codul cum trebuie 
+console.log(`Bonjour ${prenom} ${nom},dans un an vous aurez!${ageFututr}`);
